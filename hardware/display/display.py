1fcdecddd9a2144452e30ec535ea25996f31b030
Joan Code <none>
2025-02-27 20:12:28 +0100

    try and error

--- hardware/display/display.py
+++ hardware/display/display.py
@@ -1,11 +1,13 @@
 from .PCF8574 import *
 from .Adafruit_LCD1602 import Adafruit_CharLCD
 from .weather_codes import get_weather_description
+from time import sleep
 
 mcp = PCF8574_GPIO(0x27)  # Create GPIO adapter
 lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
 
 def set_lcd(water_level):
+    time.sleep(10)
     lcd.clear()
     lcd.setCursor(0,0)
     lcd.message(f'Bodenfeuchtigkeit:\n{water_level}')
