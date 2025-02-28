fd9a348eee1f7339d2705a972f9c35d4b81e7f6e
Joan Code <none>
2025-02-28 21:07:42 +0100

    adding some things

--- hardware/display/display.py
+++ hardware/display/display.py
@@ -1,15 +1,32 @@
 from .PCF8574 import *
 from .Adafruit_LCD1602 import Adafruit_CharLCD
 from .weather_codes import get_weather_description
+from time import sleep, strftime
+from datetime import datetime
 
 mcp = PCF8574_GPIO(0x27)  # Create GPIO adapter
 lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
 
+
+def get_time_now():     # get system time
+    return datetime.now().strftime('    %H:%M:%S')
+
+
+
 def set_lcd(water_level):
+    start_time = datetime.now()
+    
     mcp.output(3,1)     # Turn on backlight
     lcd.begin(16,2)     # Initialize 16x2 LCD
     lcd.clear()
     lcd.display()
     lcd.setCursor(0,0)
     lcd.message(f'Erdfeuchtigkeit:\n{water_level}')
-    return None
+    sleep(15)
+    lcd.clear()
+    lcd.display()
+    
+    while (datetime.now() - start_time).total_seconds() < 30:
+        lcd.setCursor(0,0)
+        lcd.message(get_time_now())   # display the time
+        sleep(1)
